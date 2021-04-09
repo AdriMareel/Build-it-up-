ceffdb358f030190a76d5a7abdcacb407721cbd3
Adrien Mareel <mareel.adri@yahoo.com>
2021-04-09 16:59:35 +0200

    Tests Phaser

--- README.md
+++
@@ -1,2 +0,0 @@
-# Build-it-up-
-CIRious Game Project CIR2 ISEN Lille
--- front/app.js
+++ front/app.js
@@ -1,1 +1,25 @@
-var game = new Phaser.Game(500, 300, Phaser.AUTO);
+const config = {
+	width : 1000,
+	height : 700,
+	type : Phaser.AUTO,
+	scene : {
+		preload : preload,
+		create : create,
+		update : update
+	}
+}
+
+var game = new Phaser.Game(config);
+
+function preload(){
+	this.load.image('building','images/building.png');
+	this.load.image('mario','images/mario.png');
+}
+
+function create(){
+	this.add.image(300,300,'mario');
+}
+
+function update(){
+
+}
