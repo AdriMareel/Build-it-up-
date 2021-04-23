20ee56c21563158aeedabc2a9484b3e90849b231
Adrien Mareel <mareel.adri@yahoo.com>
2021-04-23 11:50:19 +0200

    création buildingList

---
+++ front/js/buildingList.js
@@ -0,0 +1,5 @@
+let buildingList = [
+	{id: 0,name: "Mairie",price : 0,income : 0,ecologie:0,economie:0,bienEtre:0},
+	{id: 1,name : "Commissariat",price : 500,income : -50,ecologie : 0, economie : 0,bienEtre : 2.5},
+	{id: 2,name : "Poste", price : 500,income : -50,ecologie : 0,economie:0,bienEtre : 2.5}
+]
