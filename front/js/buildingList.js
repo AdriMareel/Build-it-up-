d7104c75a485453018754832229a69c0be77c89b
Adrien Mareel <mareel.adri@yahoo.com>
2021-04-28 14:07:59 +0200

    modifs

--- front/js/buildingList.js
+++ front/js/buildingList.js
@@ -1,5 +1,11 @@
 let buildingList = [
 	{id: 0,name: "Mairie",price : 0,income : 0,ecologie:0,economie:0,bienEtre:0},
-	{id: 1,name : "Commissariat",price : 500,income : -50,ecologie : 0, economie : 0,bienEtre : 2.5},
-	{id: 2,name : "Poste", price : 500,income : -50,ecologie : 0,economie:0,bienEtre : 2.5}
-]
+	{id: 1,name : "Maison",price: 250, income : 50, ecologie : 0,economie : 0, bienEtre : 0,population : 10},
+	{id: 2,name : "Commissariat",price : 500,income : -50,ecologie : 0, economie : 0,bienEtre : 2.5},
+	{id: 3,name : "Poste", price : 500,income : -50,ecologie : 0,economie:0,bienEtre : 2.5},
+	{id: 4,name	: "Hôpital", price : 1000, income : -100, ecologie : 0, economie : 0, bienEtre : 5},
+	{id: 5,name : "Centrale à charbon", price : 10000, income : 0,ecologie:-10,economie : 5,bienEtre : -5},
+	{id: 6,name : "Parc", price : 1000, income : 0,ecologie:10,economie:0,bienEtre:10},
+	{id: 7,name	: "Centrale Hydraulique", price: 10000, income : 0, ecologie : 10,economie : 0, bienEtre: 0},
+	{id: 8,name : "Usine "}
+];
