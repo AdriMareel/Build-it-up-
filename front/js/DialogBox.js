040ed231f1c39ec19dd9da4ad75a7bde6d4454da
Lucas <50884701+Atroooo@users.noreply.github.com>
2021-04-30 12:20:36 +0200

    Update DialogBox

--- front/js/DialogBox.js
+++ front/js/DialogBox.js
@@ -2,38 +2,61 @@ class DialogBox extends Phaser.Scene{
 	constructor(){
 		super('DialogBox');
 	}
-
+    
     create(){
         this.girl = this.add.sprite(game.config.width * 0.15, game.config.height * 0.70, "girl");
         this.girl.displayWidth = 500;
         this.girl.scaleY = this.girl.scaleX;
-
         this.girl.depth = 100;
  
-		    let content = [
-        "je suce des bites"
-        ];
+        let label = 'salut les mecs';
+        let label2 = 'vous etes trop beau wouhou';
+
+        let line = [];
+
+        this.label = this.add.text(500, 600).setWordWrapWidth(400);
+
+        this.typewriteTextWrapped(label);
 
         this.box = this.add.sprite(game.config.width * 0.20, game.config.height * 0.50, "box");
+        //set the width of the sprite
         this.box.displayWidth = 500;
+        //scale evenly
         this.box.scaleX = 1;
         this.box.depth = 120;
         this.box.setOrigin(0,0);
 
-        let graphics = this.make.graphics();
-
-        graphics.fillRect(152, 133, 320, 250);
-
-        let mask = new Phaser.Display.Masks.GeometryMask(this, graphics);
-
-        let text = this.add.text(500, 600, content, {  font: '60px Arial', color: '#000000', wordWrap: { width: 310 } }).setOrigin(0);
-        text.setMask(mask);
+        this.input.once('pointerdown', function () {
+            this.label.setVisible(false);
+        });
 
 
         this.input.once('pointerdown', function () {
             this.scene.sleep('DialogBox');
             this.scene.resume('MainScene');
+
         }, this);
     }
-};
+        
+    typewriteText(text){
+        const length = text.length;
+        let i = 0;
+        this.time.addEvent({
+            callback: () => {
+                this.label.text += text[i];
+                ++i;
+        },
+        repeat: length - 1,
+        delay: 100
+    });
+
+}
+    typewriteTextWrapped(text){
+        const lines = this.label.getWrappedText(text)
+        const wrappedText = lines.join('\n')
+        this.typewriteText(wrappedText)
+}
+
+    nextLine(){}
+}
 
