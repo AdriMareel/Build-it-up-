7b828ad45fca4008898652223487826dad495f09
Lucas <50884701+Atroooo@users.noreply.github.com>
2021-05-20 23:05:58 +0200

    Modif HUD

--- front/js/HUDScene.js
+++ front/js/HUDScene.js
@@ -178,37 +178,48 @@ class HUDBuildingListEcologie extends Phaser.Scene{
     this.closeButton.setInteractive({  useHandCursor: true});
     this.closeButton.on('pointerdown', () => this.close());
 
+    //Text Bank 2
+    this.textBank2 = this.add.text(1500, 70, "Vous avez : " + statistiques.getBank()/1000 + 'k', { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+
     //Panneau Solaire
-    this.buttonBuildingPSolaire = this.add.image(-800,100, 'panneau_solaire');
+    this.buttonBuildingPSolaire = this.add.image(-1100,100, 'panneau_solaire');
     this.buttonBuildingPSolaire.setInteractive({  useHandCursor: true});
     this.buttonBuildingPSolaire.on('pointerdown', () => this.placeBuildingEcologie('panneau_solaire'));
     this.buttonBuildingPSolaire.setOrigin(-3, 0);
     this.buttonBuildingPSolaire.setScrollFactor(0);
     this.buttonBuildingPSolaire.fixedToCamera = true;
+    this.prixPS = this.add.text(630, 460, buildingListMk1[7].price +"$", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+    this.textPS = this.add.text(590, 480, "Panneau Solaire", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
 
     //Centrale Hydrolique
-    this.buttonBuildingCentraleHydrolique = this.add.image(50,100, 'centrale_hydrolique1');
+    this.buttonBuildingCentraleHydrolique = this.add.image(-50,110, 'centrale_hydrolique1');
     this.buttonBuildingCentraleHydrolique.setInteractive({  useHandCursor: true});
     this.buttonBuildingCentraleHydrolique.on('pointerdown', () => this.placeBuildingEcologie('centrale_hydrolique1'));
     this.buttonBuildingCentraleHydrolique.setOrigin(-3, 0);
     this.buttonBuildingCentraleHydrolique.setScrollFactor(0);
     this.buttonBuildingCentraleHydrolique.fixedToCamera = true;
+    this.prixHydro = this.add.text(1300, 460, buildingListMk1[6].price +"$", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+    this.textHydro = this.add.text(1240, 480, "Centrale Hydrolique", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
 
     //Parc
-    this.buttonBuildingParc = this.add.image(-400,200, 'parc1');
+    this.buttonBuildingParc = this.add.image(-200,500, 'parc1');
     this.buttonBuildingParc.setInteractive({  useHandCursor: true});
     this.buttonBuildingParc.on('pointerdown', () => this.placeBuildingEcologie('parc1'));
     this.buttonBuildingParc.setOrigin(-3, 0);
     this.buttonBuildingParc.setScrollFactor(0);
     this.buttonBuildingParc.fixedToCamera = true;
+    this.prixParc = this.add.text(620, 740, buildingListMk1[8].price +"$", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+    this.textParc = this.add.text(625, 760, "Parc", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
 
     //Usine Recyclage
-    this.buttonBuildingUsineRecyclage = this.add.image(100,500, 'usine_recyclage1');
+    this.buttonBuildingUsineRecyclage = this.add.image(500,520, 'usine_recyclage1');
     this.buttonBuildingUsineRecyclage.setInteractive({  useHandCursor: true});
     this.buttonBuildingUsineRecyclage.on('pointerdown', () => this.placeBuildingEcologie('usine_recyclage1'));
     this.buttonBuildingUsineRecyclage.setOrigin(-3, 0);
     this.buttonBuildingUsineRecyclage.setScrollFactor(0);
     this.buttonBuildingUsineRecyclage.fixedToCamera = true;
+    this.prixRecy = this.add.text(1340, 740, buildingListMk1[9].price +"$", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+    this.textRecy = this.add.text(1300, 760, "Usine Recyclage", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
   }
 
   placeBuildingEcologie(name){
@@ -234,61 +245,78 @@ class HUDBuildingListMoral extends Phaser.Scene {
       this.closeButton.setInteractive({  useHandCursor: true});
       this.closeButton.on('pointerdown', () => this.close());
 
+      //Text Bank 2
+      this.textBank2 = this.add.text(1500, 70, "Vous avez : " + statistiques.getBank()/1000 + 'k', { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+
       //Caserne
-      this.buttonBuildingCaserne = this.add.image(600,200, 'caserne1');
+      this.buttonBuildingCaserne = this.add.image(650,180, 'caserne1');
       this.buttonBuildingCaserne.setInteractive({useHandCursor: true});
       this.buttonBuildingCaserne.on('pointerdown', () => this.placeBuildingMoral('caserne1'));
       this.buttonBuildingCaserne.setOrigin(-3, 0);
       this.buttonBuildingCaserne.setScrollFactor(0);
       this.buttonBuildingCaserne.fixedToCamera = true;
+      this.prixCaserne = this.add.text(1500, 440, buildingListMk1[0].price +"$", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+      this.textCaserne = this.add.text(1492, 460, "Caserne", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
 
       //Maison
-      this.buttonBuildingMaison = this.add.image(100, 50, 'maison1');
+      this.buttonBuildingMaison = this.add.image(150, 500, 'maison1');
       this.buttonBuildingMaison.setInteractive({useHandCursor: true});
       this.buttonBuildingMaison.on('pointerdown', () => this.placeBuildingMoral('maison1'));
       this.buttonBuildingMaison.setOrigin(-3, 0);
       this.buttonBuildingMaison.setScrollFactor(0);
       this.buttonBuildingMaison.fixedToCamera = true;
+      this.prixMaison = this.add.text(970, 750, buildingListMk1[14].price +"$", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+      this.textMaison = this.add.text(965, 770, "Maison", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
 
       //Centrale
-      this.buttonBuildingCentrale = this.add.image(50,250, 'centrale1');
+      this.buttonBuildingCentrale = this.add.image(500,500, 'centrale1');
       this.buttonBuildingCentrale.setInteractive({useHandCursor: true});
       this.buttonBuildingCentrale.on('pointerdown', () => this.placeBuildingMoral('centrale1'));
       this.buttonBuildingCentrale.setOrigin(-3, 0);
       this.buttonBuildingCentrale.setScrollFactor(0);
       this.buttonBuildingCentrale.fixedToCamera = true;
+      this.prixCentrale = this.add.text(1350, 750, buildingListMk1[1].price +"$", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+      this.textCentrale = this.add.text(1340, 770, "Centrale", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
 
       //Commissariat
-      this.buttonBuildingCommissariat = this.add.image(-1100,350, 'commissariat1');
+      this.buttonBuildingCommissariat = this.add.image(250,180, 'commissariat1');
       this.buttonBuildingCommissariat.setInteractive({useHandCursor: true});
       this.buttonBuildingCommissariat.on('pointerdown', () => this.placeBuildingMoral('commissariat1'));
       this.buttonBuildingCommissariat.setOrigin(-3, 0);
       this.buttonBuildingCommissariat.setScrollFactor(0);
       this.buttonBuildingCommissariat.fixedToCamera = true;
+      this.prixCommi = this.add.text(1100, 440, buildingListMk1[2].price +"$", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+      this.textCommi = this.add.text(1070, 460, "Commissariat", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
 
       //Ecole
-      this.buttonBuildingEcole = this.add.image(-400,200, 'ecole1');
+      this.buttonBuildingEcole = this.add.image(-250,500, 'ecole1');
       this.buttonBuildingEcole.setInteractive({useHandCursor: true});
       this.buttonBuildingEcole.on('pointerdown', () => this.placeBuildingMoral('ecole1'));
       this.buttonBuildingEcole.setOrigin(-3, 0);
       this.buttonBuildingEcole.setScrollFactor(0);
       this.buttonBuildingEcole.fixedToCamera = true;
+      this.prixEcole = this.add.text(550, 750, buildingListMk1[3].price +"$", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+      this.textEcole = this.add.text(552, 770, "Ecole", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
 
       //Hopital
-      this.buttonBuildingHopital = this.add.image(700,500, 'hopital1');
+      this.buttonBuildingHopital = this.add.image(-450,180, 'hopital1');
       this.buttonBuildingHopital.setInteractive({useHandCursor: true});
       this.buttonBuildingHopital.on('pointerdown', () => this.placeBuildingMoral('hopital1'));
       this.buttonBuildingHopital.setOrigin(-3, 0);
       this.buttonBuildingHopital.setScrollFactor(0);
       this.buttonBuildingHopital.fixedToCamera = true;
+      this.prixHopital = this.add.text(350, 440, buildingListMk1[4].price +"$", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+      this.textHopital = this.add.text(345, 460, "Hopital", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
 
       //Poste
-      this.buttonBuildingPoste = this.add.image(-400,500, 'poste1');
+      this.buttonBuildingPoste = this.add.image(-100,180, 'poste1');
       this.buttonBuildingPoste.setInteractive({useHandCursor: true});
       this.buttonBuildingPoste.on('pointerdown', () => this.placeBuildingMoral('poste1'));
       this.buttonBuildingPoste.setOrigin(-3, 0);
       this.buttonBuildingPoste.setScrollFactor(0);
       this.buttonBuildingPoste.fixedToCamera = true;
+      this.prixPoste = this.add.text(750, 440, buildingListMk1[5].price +"$", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+      this.textPoste = this.add.text(752, 460, "Poste", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
     }
 
     placeBuildingMoral(name){
@@ -314,21 +342,28 @@ class HUDBuildingListEconomie extends Phaser.Scene{
     this.closeButton.setInteractive({  useHandCursor: true});
     this.closeButton.on('pointerdown', () => this.close());
 
+    //Text Bank 2
+    this.textBank2 = this.add.text(1500, 120,"Vous avez : " + statistiques.getBank()/1000 + 'k', { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+
     //Banque
-    this.buttonBuildingBanque = this.add.image(-800,200, 'banque1');
+    this.buttonBuildingBanque = this.add.image(-750,150, 'banque1');
     this.buttonBuildingBanque.setInteractive({  useHandCursor: true});
     this.buttonBuildingBanque.on('pointerdown', () => this.placeBuildingEconomie('banque1'));
     this.buttonBuildingBanque.setOrigin(-3, 0);
     this.buttonBuildingBanque.setScrollFactor(0);
     this.buttonBuildingBanque.fixedToCamera = true;
+    this.prixBanque = this.add.text(970, 650, buildingListMk1[10].price +"$", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+    this.textBanque = this.add.text(960, 675, "Banque", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
 
     //Magasin
-    this.buttonBuildingMagasin = this.add.image(-400,300, 'magasin1');
+    this.buttonBuildingMagasin = this.add.image(-350,300, 'magasin1');
     this.buttonBuildingMagasin.setInteractive({  useHandCursor: true});
     this.buttonBuildingMagasin.on('pointerdown', () => this.placeBuildingEconomie('magasin1'));
     this.buttonBuildingMagasin.setOrigin(-3, 0);
     this.buttonBuildingMagasin.setScrollFactor(0);
     this.buttonBuildingMagasin.fixedToCamera = true;
+    this.prixMagasin = this.add.text(500, 550, buildingListMk1[11].price +"$", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+    this.textMagasin = this.add.text(490, 575, "Magasin", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
 
     //Usine
     this.buttonBuildingUsine = this.add.image(550,300, 'usine1');
@@ -337,6 +372,8 @@ class HUDBuildingListEconomie extends Phaser.Scene{
     this.buttonBuildingUsine.setOrigin(-3, 0);
     this.buttonBuildingUsine.setScrollFactor(0);
     this.buttonBuildingUsine.fixedToCamera = true;
+    this.prixUsine = this.add.text(1400, 550, buildingListMk1[12].price +"$", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
+    this.textUsine = this.add.text(1400, 575, "Usine", { fill: 0xffffff, font: 'bold 18px system-ui' }).setShadow(2, 2, 0xffff00, 8);
   }
 
   placeBuildingEconomie(name){
